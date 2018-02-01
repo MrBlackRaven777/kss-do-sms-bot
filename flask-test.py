f5243c7b81d73d8de70ee047ddd0bfc16349d0d1
BlackRaven <cakeman3000@gmail.com>
2018-02-01 16:11:29 +0300

    0000

--- flask-test.py
+++ flask-test.py
@@ -29,4 +29,4 @@ def webhook():
     return "!", 200
 
 if __name__ == '__main__':
-    server.run(host='127.0.0.1', port=int(os.environ.get('PORT', 5000)), debug=True)
+    server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
