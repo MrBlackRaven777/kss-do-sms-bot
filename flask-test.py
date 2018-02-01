0d77d7291cad2a3b0453ff80e7e17fcfc451ce14
BlackRaven <cakeman3000@gmail.com>
2018-02-01 13:23:14 +0300

    change url

--- flask-test.py
+++ flask-test.py
@@ -1,8 +1,9 @@
 import telebot
 import os
+import config
 from flask import Flask, request
 
-bot = telebot.TeleBot('<api_token>')
+bot = telebot.TeleBot(config.token)
 
 server = Flask(__name__)
 
@@ -22,7 +23,7 @@ def getMessage():
 @server.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url="https://herokuProject_url/bot")
+    bot.set_webhook(url="https://dashboard.heroku.com/apps/kss-do-sms-bot/")
     return "!", 200
 
 server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
