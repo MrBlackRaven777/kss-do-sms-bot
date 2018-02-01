0bb69b2384fbf7e3941c67a2e37ccb68cfbd0ab8
MrBlackRaven777 <cakeman3000@gmail.com>
2018-02-01 20:43:46 +0300

    chat.id

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ server = Flask(__name__)
 @bot.message_handler(commands=['start'])
 def start(message):
     answer = 'Приветствую тебя, %s! Я бот для управления смс-рассылкой :ООО"КомпозитСпецСтрой". Отправь мне /balance чтобы узнать о текущем состоянии счета.'%(message.from_user.first_name)
-    bot.send_message(message.chat_id, answer)
+    bot.send_message(message.chat.id, answer)
 
 @bot.message_handler(commands=['balance'])
 def balance(message):
@@ -19,12 +19,12 @@ def balance(message):
     if result.json().get('status') == 'OK' and result.json().get('status_code') == 100:
         balance = int(result.json().get('balance'))
         answer = 'Ваш баланс: ' + str(balance) + 'р.'
-    bot.send_message(message.chat_id, answer)
+    bot.send_message(message.chat.id, answer)
 
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo(message):
     answer = 'К сожалению, я не знаю эту команду. Но в будущем я собираюсь расширить свой функционал'
-    bot.send_message(message.chat_id, answer)
+    bot.send_message(message.chat.id, answer)
 
 @server.route("/"+config.token, methods=['GET', 'POST'])
 def getMessage():
